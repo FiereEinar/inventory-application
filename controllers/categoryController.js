aa82c1ec65d5351c768751528aa75b5e1f0203f9
FiereEinar <nickxylanmelloria@gmail.com>
2024-05-27 17:27:15 +0800

    feature: add, edit, delete actions on categories

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,6 +1,8 @@
 const Item = require('../models/item');
 const Category = require('../models/category');
 const asyncHandler = require("express-async-handler");
+const { body, validationResult } = require('express-validator');
+const item = require('../models/item');
 
 exports.category_list = asyncHandler(async (req, res) => {
   const categories = await Category.find().sort({ name: 1 }).exec();
@@ -28,3 +30,112 @@ exports.category_detail = asyncHandler(async (req, res) => {
   });
 });
 
+exports.category_add_get = (req, res) => {
+  res.render('category_views/category_add_form', {
+    title: 'Add Category'
+  });
+};
+
+exports.category_add_post = [
+  body('name', 'Name must be 2 characters above and 50 characters below')
+    .trim()
+    .isLength({ min: 2, max: 50 })
+    .escape(),
+
+  asyncHandler(async (req, res) => {
+    const errors = validationResult(req);
+
+    const category = new Category({
+      name: req.body.name
+    });
+
+    if (!errors.isEmpty()) {
+      res.render('category_views/category_add_form', {
+        title: 'Add Category',
+        category: category,
+        errors: errors.array()
+      });
+    } else {
+      await category.save();
+      res.redirect('/home/categories');
+    }
+  })
+];
+
+exports.category_edit_get = asyncHandler(async (req, res) => {
+  const category = await Category.findById(req.params.id).exec();
+
+  res.render('category_views/category_add_form', {
+    title: 'Edit Category',
+    category: category
+  });
+});
+
+exports.category_edit_post = [
+  body('name', 'Name must be 2 characters above and 50 characters below')
+    .trim()
+    .isLength({ min: 2, max: 50 })
+    .escape(),
+
+  asyncHandler(async (req, res) => {
+    const errors = validationResult(req);
+
+    const category = new Category({
+      name: req.body.name,
+      _id: req.params.id
+    });
+
+    if (!errors.isEmpty()) {
+      res.render('category_views/category_add_form', {
+        title: 'Edit Category',
+        category: category,
+        errors: errors.array()
+      });
+    } else {
+      const updatedCategory = await Category.findByIdAndUpdate(req.params.id, category, {});
+      res.redirect(updatedCategory.url);
+    }
+  })
+];
+
+exports.category_delete_get = asyncHandler(async (req, res) => {
+  const [category, categoryItems] = await Promise.all([
+    Category.findById(req.params.id).exec(),
+    Item.find({ category: req.params.id }).populate('category').sort({ name: 1 }).exec()
+  ]);
+
+  const num_of_items = categoryItems.length;
+
+  if (category === null) {
+    const error = new Error('Category not found');
+    error.status = 404;
+    return next(error);
+  }
+
+  res.render('category_views/delete_category', {
+    title: 'Delete Category',
+    category: category,
+    categoryItems: categoryItems,
+    num_of_items: num_of_items
+  });
+});
+
+exports.category_delete_post = asyncHandler(async (req, res) => {
+  const categoryItems = await Item.find({ category: req.params.id }).populate('category').sort({ name: 1 }).exec();
+
+  if (categoryItems.length) {
+    const category = await Category.findById(req.params.id).exec();
+
+    const num_of_items = categoryItems.length;
+
+    res.render('category_views/delete_category', {
+      title: 'Delete Category',
+      category: category,
+      categoryItems: categoryItems,
+      num_of_items: num_of_items
+    });
+  } else {
+    await Category.findByIdAndDelete(req.params.id);
+    res.redirect('/home/categories');
+  }
+});
--- routes/home.js
+++ routes/home.js
@@ -29,6 +29,18 @@ router.get('/item/:id', itemController.item_detail);
 // category routes
 router.get('/categories', categoryController.category_list);
 
+router.get('/category/add', categoryController.category_add_get);
+
+router.post('/category/add', categoryController.category_add_post);
+
+router.get('/category/:id/edit', categoryController.category_edit_get);
+
+router.post('/category/:id/edit', categoryController.category_edit_post);
+
+router.get('/category/:id/delete', categoryController.category_delete_get);
+
+router.post('/category/:id/delete', categoryController.category_delete_post);
+
 router.get('/category/:id', categoryController.category_detail);
 
 module.exports = router;
